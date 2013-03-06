43e1408479665df7fbd19231438b4012627111d0
Amir <amirvalizadeh81@yahoo.com>
2013-03-06 12:15:18 -0800

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# Return a title for each page
+	def title
+		base_title = "ROR sample"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
